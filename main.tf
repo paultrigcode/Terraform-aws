f50cf45c1bfa2005e8768624b0b573891d9a1e0b
Paul <mailpaulade@gmail.com>
2022-11-16 15:43:01 +0100

    added remote backend

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "paultrig"
+
+    workspaces {
+      name = "paultrig"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.39.0"
+    }
+  }
+}
+
+
 locals {
   project_name = "MyServer"
 }
--- providers.tf
+++ providers.tf
@@ -1,13 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.39.0"
-    }
-  }
-}
-
 provider "aws" {
   region  = "us-east-1"
-  profile = "default"
+#   profile = "default"
 }
